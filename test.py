3d17c6de24d964cc1475d1fc9db86b774095ee2c
Mohamed TRIKI <trikimed1997@gmail.com>
2023-01-25 20:54:48 +0100

    Exception added.

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 import time
-class testClass: 
+
+
+class testClass:
 
     def test_function():
         phases = int(input("Test phases: "))
         print("! Test is starting execution ! ")
-        for i in range(phases):
-            print("   Test phase " + str(i) + " running...")
-            time.sleep(1)
-        print("! Test completed !")
+        try:
+            for i in range(phases):
+                print("   Test phase " + str(i+1) + " running...")
+                time.sleep(1)
+            print("! Test completed !")
 
-if __name__ ==  __name__:
+        except:
+            print("Something went wrong, please heck input !")
+
+
+if __name__ == __name__:
     testClass.test_function()
-
